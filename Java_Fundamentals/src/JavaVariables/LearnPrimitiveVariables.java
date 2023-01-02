611948305ab420426066d3e6e5efbf5803f7988d
karthinerd <106096524+karthinerd@users.noreply.github.com>
2023-01-02 23:09:04 +0530

    Day 2 Primitive Variables

---
+++ Java_Fundamentals/src/JavaVariables/LearnPrimitiveVariables.java
@@ -0,0 +1,34 @@
+package JavaVariables;
+
+public class LearnPrimitiveVariables {
+
+	public static void main(String[] args) {
+
+		int myNumber = 12 ;
+		
+		short myShort = 666;
+		
+		long myLong = 23345;
+		
+		float myFloat = 34.66f;
+		
+		double myDouble = 12353;
+		
+		byte myByte = 127;
+		
+		char myChar = 'k';
+		
+		boolean myBoolean = true ;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myByte);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+	}
+
+}
